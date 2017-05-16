df37119f26b541b64a85cc8d75b56d3e78a45578
Jake Iwen <iwenx006@umn.edu>
2017-05-16 18:34:10 -0500

    get history and parse

--- history/history.rb
+++ history/history.rb
@@ -3,6 +3,9 @@ require 'pry'
 require 'json'
 require 'pp'
 require 'dotenv'
+require 'open-uri'
+require "byebug"
+require 'rest-client'
 
 Dotenv.load('../.env')
 
@@ -33,73 +36,44 @@ client.on :message do |data|
       def initialize(client, channel)
         @channel = channel
         @client = client
+        @json_url = []
         @old_history = []
         @ts = 0
       end
 
       def write_new_history
-        get_old_history
+        @old_history = fetch_and_parse_history
         get_time_stamp
         @new_history = @client.web_client.channels_history(channel: @channel, oldest: @ts).messages
-        File.open("history.json", "w") do |f|
-          f.puts JSON.pretty_generate(@new_history + @old_history)
-        end
+        byebug
+        # File.open("history.json", "w") do |f|
+        #   f.puts JSON.pretty_generate(@new_history + @old_history)
+        # end
       end
 
       private
-
-      def get_old_history
-        @file_obj_list = @client.web_client.files_list.files
-        pp @file_obj_list
-
-        @file_name_list = @file_obj_list.map { |a|
-          { "file_name" => a["name"], "file_url" => a["permalink_public"] }
-        }
-        if @file_name_list.include?('slack-stack-history')
-          @msg_json = @file_name_list.select { |a| a=='slack-stack-history' }
-        else
-          puts "no message history json found. create new?"
-        end
-        pp @file_name_list
-        # binding.pry
-        begin
-          @old_history = JSON.parse(File.read('history.json'))
-        rescue
-          puts 'An error occured parsing the history file'
-        end
-      end
 
       def get_time_stamp
         return unless @old_history.first.is_a? Hash
         return unless @old_history.first['ts']
         @ts = @old_history.first['ts'].to_f + 1
       end
+
+      def fetch_and_parse_history
+        file_obj = @client.web_client.files_list.files.select{ |k|  k["title"].to_s.match("slack-stack-history") }
+        @json_url = file_obj[0]["url_private_download"]
+        res = RestClient.get(@json_url, { "Authorization" => "Bearer #{ENV['SLACK_API_TOKEN']}" })
+        return res_code unless res.code == 200
+        JSON.parse(res.body)
+      end
     end
 
     HistoryGenerator.new(client, data.channel).write_new_history
 
-    # If history.json exists AND has valid hash ['ts'] at parsed array position 0, only get new messages
-    # append new messages to existing messages
-    # write new history.json
-
-    # is there a better way to protect against non-existing/bad files?
-    # json = File.read('history.json') rescue nil
-    # oldhist = JSON.parse(json) rescue []
-    # if oldhist[0].nil? || oldhist[0]['ts'].nil?
-    #   puts "invalid/empty existing file"
-    #   ts =  0
-    #   oldhist = []
-    # else
-    #   puts "valid existing file"
-    #   ts =  oldhist[0]['ts'].to_f + 1
-    # end
-    # newhist = client.web_client.channels_history(channel: data.channel, oldest:ts).messages
-    #
-    # File.open("history.json", "w") do |f|
-    #   # gets ALL messages if ts==0:
-    #   f.puts JSON.pretty_generate(newhist + oldhist)
-    # end
   end
 end
+
+
+
 
 client.start!
