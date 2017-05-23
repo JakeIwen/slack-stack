0500192b7aea9a138d5d28d0b9a4cc8608131c99
Jake Iwen <iwenx006@umn.edu>
2017-05-22 19:29:05 -0500

    channel specific

--- history/history.rb
+++ history/history.rb
@@ -1,5 +1,4 @@
 require 'slack-ruby-client'
-require 'pry'
 require 'json'
 require 'pp'
 require 'dotenv'
@@ -22,18 +21,17 @@ class HistoryGenerator
 
   def generate
     fetch_history
-    return false if @file_objects.empty?
-    return false unless parse_history
+    parse_history unless @file_objects.empty?
     get_time_stamp
-    success = post_new_history?
-    delete_old_history if success
-    true
+    url = post_new_history
+    delete_old_history if url
+    url
   end
 
   private
 
   def fetch_history
-    @file_objects = @client.files_list.files.select{ |k|  k["title"].to_s.match("slack-stack-history") }
+    @file_objects = @client.files_list.files(channel: @channel).select{ |k|  k["title"].to_s.match("slack-stack-history") }
   end
 
   def parse_history
@@ -49,11 +47,11 @@ class HistoryGenerator
     @ts = @old_history.first['ts'].to_f + 1
   end
 
-  def post_new_history?
+  def post_new_history
     new_messages = @client.channels_history(channel: @channel, oldest: @ts).messages
     message_json = JSON.pretty_generate(new_messages + @old_history)
     comment = Time.at(@ts).to_s + " to " + Time.now.to_s
-    @client.files_upload(content:message_json, filetype:"javascript", title:"slack-stack-history", filename:"history.json", initial_comment:comment, user:@user).ok
+    @client.files_upload(content:message_json, filetype:"javascript", title:"slack-stack-history", filename:"history.json", initial_comment:comment, user:@user, channel:@channel).file["url_private_download"] rescue false
   end
 
   def delete_old_history
@@ -83,12 +81,15 @@ client.on :message do |data|
   when /^bot/ then
     client.web_client.chat_postMessage channel: data.channel, text: "Sorry <@#{data.user}>, what?"
   when 'history' then
-    history_written = HistoryGenerator.new(client, data.channel, data.user).generate
-    if history_written
+    history_written_url = HistoryGenerator.new(client, data.channel, data.user).generate
+    if history_written_url
       puts "Okay, I wrote your history"
+      client.web_client.chat_postMessage channel: data.channel, text: "Messages JSON: #{history_written_url}"
     else
       puts "Sorry, something went wrong."
     end
+  when 'archive' then
+    client.web_client.channels_archive channel: data.channel, text: "Archiving!"
   end
 end
 
